eb923689a21b3b89f17cd1634e71ed789808abf7
FarukKalil <faruk1.kalil@gmail.com>
2023-03-20 13:31:21 -0300

    Exercicio 10 finalizado

--- Exercicio10/src/exercicio10/Exercicio10.java
+++ Exercicio10/src/exercicio10/Exercicio10.java
@@ -1,9 +1,34 @@
 package exercicio10;
 
+import java.util.Scanner;
+
 public class Exercicio10 {
 
     public static void main(String[] args) {
+        String nomeFuncionario;
+        double salBruto, salLiquido, horasTrab, valorHTrab;
+        double inss = 1.10;
+        Scanner entradaDados = new Scanner(System.in);
+        System.out.println("Programa:::");
+        System.out.println("Calcula Salário Bruto e Salário Líquido");
+        System.out.println("\nEntre com o nome do Funcionário: ");
+        nomeFuncionario = entradaDados.nextLine();
         
+        System.out.println("Informe a Quantidade de horas Trabalhadas: ");
+        horasTrab = entradaDados.nextDouble();
+        
+        System.out.println("Informe o valor das horas trabalhadas: R$ ");
+        valorHTrab = entradaDados.nextDouble();
+        
+        salBruto = (horasTrab * valorHTrab);
+        salLiquido = (salBruto - inss);
+        System.out.println("\n\n:::: Exibição dos dados informados ::::::::::");
+        System.out.println("Nome: "+nomeFuncionario);
+        System.out.printf("Salário Bruto: R$ %.2f", salBruto);
+        System.out.printf("/nSalário Líquido: R$ %.2f", salLiquido);
+        System.out.println("/\n:::::::::::::::::::::::::::::::::::::::::::::");
+        
+        System.out.println("Fim do Programa!!!");
     }
     
 }
